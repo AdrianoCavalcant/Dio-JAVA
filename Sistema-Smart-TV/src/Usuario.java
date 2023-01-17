3c3c8573090bbc81dcea2acd945d7da84997f74d
AdrianoCavalcant <silvaadriano233@gmail.com>
2023-01-17 10:17:31 -0300

    Calculadora/Comentarios

--- Sistema-Smart-TV/src/Usuario.java
+++ Sistema-Smart-TV/src/Usuario.java
@@ -9,13 +9,20 @@ public class Usuario {
         smartTv.aumentarVolume();
         System.out.println("Volume Atual : " + smartTv.volume);
 
+
         System.out.println("Tv Ligada ? " + smartTv.ligada);
         System.out.println("Canal atual " + smartTv.canal);
         System.out.println("Volume do canal " + smartTv.volume);
+
+        smartTv.mudarCanal(10);
+        System.out.println("Canal atual ? " + smartTv.canal);
     
 
         smartTv.ligar ();
         System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Desligada ? " + smartTv.ligada);
 
     }
 
