968f86f54ef45104434a17c38c237f923531151b
AdrianoCavalcant <silvaadriano233@gmail.com>
2023-01-23 19:07:19 -0300

    desafios

---
+++ Desafios/src/mesadaDoSobrinho.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class mesadaDoSobrinho {
+
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+       
+        int entrada;
+        int mesada = 50;
+
+        System.out.println("Quantidade de meses: " ); 
+        
+        entrada = leitor.nextInt();
+        
+         System.out.println("Você tera " + mesada * entrada + " reais");
+            
+        
+
+
+
+}  }  
